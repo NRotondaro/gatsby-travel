abc9533ea797aca11cf5435b9a13a4b2f5f149f4
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-25 22:31:56 +0000

    feat(hero): added component and header changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <Hero />
   </Layout>
 )
 
