f00c3a5578ac76e40cb22e100e25273d742ba258
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-26 23:22:04 +0000

    feat(trip): added the trip session with gatsby image plugin

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,21 +1,72 @@
 import React from "react"
 import styled from "styled-components"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Button } from "./Button"
+import { ImLocation } from "react-icons/im"
 
-const Trips = () => {
+const Trips = ({ heading }) => {
+  const data = useStaticQuery(query)
+  const trips = data.allTripsJson.nodes
+
   return (
     <ProductsContainer>
-      <ProductsHeading>Heading</ProductsHeading>
-      <ProductsWrapper>Wrapper</ProductsWrapper>
+      <ProductsHeading>{heading}</ProductsHeading>
+      <ProductsWrapper>
+        {trips.map((trip, index) => {
+          const { alt, button, name, img } = trip
+          const pathToImage = getImage(img)
+          return (
+            <ProductCard key={index}>
+              <ProductImg image={pathToImage} alt={alt} />
+              <ProductInfo>
+                <TextWrap>
+                  <ImLocation />
+                  <ProductTitle>{name}</ProductTitle>
+                </TextWrap>
+                <Button
+                  to="/trips"
+                  primary="true"
+                  rounded="true"
+                  css={`
+                    position: absolute;
+                    top: 420px;
+                    font-size: 14px;
+                  `}
+                >
+                  {button}
+                </Button>
+              </ProductInfo>
+            </ProductCard>
+          )
+        })}
+      </ProductsWrapper>
     </ProductsContainer>
   )
 }
+
+const query = graphql`
+  query TripsQuery {
+    allTripsJson {
+      nodes {
+        alt
+        button
+        name
+        img {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Trips
 
 const ProductsContainer = styled.div`
   min-height: 100vh;
   padding: 5rem calc((100vw - 1300px) / 2);
-  background: red;
   color: #fff;
 `
 const ProductsHeading = styled.div`
@@ -24,4 +75,64 @@ const ProductsHeading = styled.div`
   margin-bottom: 5rem;
   color: #000;
 `
-const ProductsWrapper = styled.div``
+const ProductsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  justify-items: center;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 868px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ProductCard = styled.div`
+  line-height: 2;
+  width: 100%;
+  height: 500px;
+  position: relative;
+  border-radius: 10px;
+  transition: 0.2s ease;
+`
+
+const ProductImg = styled(GatsbyImage)`
+  height: 100%;
+  max-width: 100%;
+  position: relative;
+  border-radius: 10px;
+  filter: brightness(70%);
+  transition: 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    filter: brightness(100%);
+  }
+`
+
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 280px) {
+    padding: 0 1rem;
+  }
+`
+
+const TextWrap = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 375px;
+`
+
+const ProductTitle = styled.div`
+  font-weight: 400;
+  font-size: 1rem;
+  margin-left: 0.5rem;
+`
